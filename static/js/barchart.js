bbf99f1c8da05f92a7a58e75a67825d0f347fe47
Creepiz <2865729331@qq.com>
2018-03-20 15:28:49 +0800

    [data enter]

--- static/js/barchart.js
+++ static/js/barchart.js
@@ -1,21 +1,39 @@
-const dataset = [50,29,23,180,123];
-const width = 200;
-const height = 200;
-const svg = d3.select("#svg").append("svg").attr("width",width).attr("height",height)
-const padding = {top:20,right:20,bottom:20,left:20}
+const dataset = [50, 29, 23, 180, 123];
+const width = 400;
+const height = 400;
+const svg = d3.select("#svg").append("svg").attr("width", width).attr("height", height)
+const padding = {
+    top: 20,
+    right: 20,
+    bottom: 20,
+    left: 20
+}
 const rectStep = 35;
-const rectWidth= 30;
+const rectWidth = 30;
+const genRect = obj => {
+    obj.attr("fill", "red")
+        .attr("x", (d, i) => padding.left + i * rectStep)
+        .attr("y", (d, i) => height - padding.bottom - d)
+        .attr("width", rectWidth)
+        .attr("height", d => d)
+}
+const genText = obj => {
+    obj.attr("fill", "#fff")
+        .attr("font-size", "14px").attr("text-anchor", "middle")
+        .attr("x", (d, i) => padding.left + i * rectStep)
+        .attr("y", (d, i) => height - padding.bottom - d)
+        .text(d => d)
+        .attr("dx", (d, i) => rectWidth / 2)
+        .attr("dy", "1em")
+}
+const init = dataset => {
+    genRect(svg.selectAll("rect").data(dataset).enter().append("rect"))
+    genText(svg.selectAll("text").data(dataset).enter().append("text"))
+}
+const update = dataset => {
+    genRect(svg.selectAll("rect").data(dataset))
+    genText(svg.selectAll("text").data(dataset))
+}
 
-svg.selectAll("rect").data(dataset).enter().append("rect").attr("fill","red")
-    .attr("x",(d,i) => padding.left+i*rectStep)
-    .attr("y",(d,i) =>height-padding.bottom-d)
-    .attr("width",rectWidth)
-    .attr("height",d => d)
-
-const text = svg.selectAll("text").data(dataset).enter().append("text").attr("fill","#fff")
-    .attr("font-size","14px").attr("text-anchor","middle")
-    .attr("x",(d,i) => padding.left + i*rectStep)
-    .attr("y",(d,i) => height -padding.bottom -d)
-    .text(d => d)
-    .attr("dx",(d,i)=> rectWidth/2)
-    .attr("dy","1em")
+init([50, 29, 23, 180, 200])
+update([50, 29, 23, 180, 100])
