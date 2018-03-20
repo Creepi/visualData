70c01417cf3a5824a1d207bf193b3c40def536fa
Creepiz <2865729331@qq.com>
2018-03-20 17:42:57 +0800

    [比例尺]

--- static/js/barchart.js
+++ static/js/barchart.js
@@ -1,5 +1,5 @@
 const dataset = [50, 29, 23, 180, 123];
-const width = 400;
+const width = 500;
 const height = 400;
 const svg = d3.select("#svg").append("svg").attr("width", width).attr("height", height)
 const padding = {
@@ -10,11 +10,25 @@ const padding = {
 }
 const rectStep = 35;
 const rectWidth = 30;
+
+//定义坐标轴
+const xAxisWidth = width - padding.left - padding.right
+const yAxisWidth = height - padding.top - padding.bottom
+const xScale = d3.scaleBand().domain(dataset.map((o, i) => i)).rangeRound([0,xAxisWidth]).padding(0.1)
+const yScale = d3.scaleLinear().domain([0,d3.max(dataset)]).rangeRound([yAxisWidth,0])
+const xAxis = d3.axisBottom(xScale)
+const gX = svg.append("g").attr("transform",`translate(${padding.left},${height - padding.bottom})`)
+gX.call(xAxis)
+const yAxis = d3.axisLeft(yScale)
+const gY = svg.append("g").attr("transform",`translate(${padding.left},${height - yAxisWidth - padding.bottom})`)
+gY.call(yAxis)
+
+const color = d3.scaleOrdinal(d3.schemeCategory10)
 const genRect = obj => {
-    obj.attr("fill", "red")
-        .attr("x", (d, i) => padding.left + i * rectStep)
+    obj.attr("fill", (d,i) => color(i))
+        .attr("x", (d, i) => padding.left + xScale(i))
         .attr("y", (d, i) => height - padding.bottom - d)
-        .attr("width", rectWidth)
+        .attr("width", xScale.bandwidth)
         .attr("height", d => d)
 }
 const genText = obj => {
@@ -35,5 +49,4 @@ const update = dataset => {
     genText(svg.selectAll("text").data(dataset))
 }
 
-init([50, 29, 23, 180, 200])
-update([50, 29, 23, 180, 100])
+init(dataset)
